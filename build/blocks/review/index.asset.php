a6e6be5c8fde5d1b12167613b0575adbc0e5aaad
Nazmul <thankyounayem1@gmail.com>
2023-05-17 09:57:18 -0700

    17 May 23

--- build/blocks/review/index.asset.php
+++ build/blocks/review/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0ed0b87d2f232d8fe794');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '570bef35cc5a2ed90925');
